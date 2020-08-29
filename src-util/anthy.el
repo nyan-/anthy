308d14b4103016dbc2629c99febd37b3cbe08a38
Yoshihiro Takahashi <nyan@furiru.org>
2020-08-29 20:19:48 +0900

    Fix for emacs 27.1.

--- src-util/anthy.el
+++ src-util/anthy.el
@@ -745,7 +745,7 @@
 	(if anthy-agent-process
 	    (kill-process anthy-agent-process))
 	(setq anthy-agent-process proc)
-	(process-kill-without-query proc)
+	(set-process-query-on-exit-flag proc nil)
 	(if anthy-xemacs
 	    (if (coding-system-p (find-coding-system 'euc-japan))
 		(set-process-coding-system proc 'euc-japan 'euc-japan))
